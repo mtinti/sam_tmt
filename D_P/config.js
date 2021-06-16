694f9ca9dc072d0fb465dba6f68d2d57c65eadd2
mtinti <michele.tinti@gmail.com>
2021-06-16 15:52:17 +0100

    start

---
+++ D_P/config.js
@@ -0,0 +1,26 @@
+let X1='Signed_Rank';
+let Y1='FDR';
+let X2='log10_TMT_intensity';
+let Y2='Signed_Rank';
+
+let FLIP_Y1=true;
+let FLIP_Y2=false;
+
+let PLOT_WIDTH=550;
+let PLOT_HEIGHT=400;
+
+let PROJECT_TITLE='DM vs Plus';
+
+let margin = 0.05;
+
+let BOX_PLOT_COLS_1 = ['DM1','DM2','DM3']
+let BOX_PLOT_COLS_2 = ['Plus1','Plus2','Plus3']
+
+let CONDITION_1 = 'DM'
+let CONDITION_2 = 'Plus'
+
+
+let COLOR_SELECTION_TABLE = 'orange';
+let COLOR_SELECTION_CIRCLE = 'orange';
+
+let Y_LABEL_BARPLOT = 'iBAQ';
